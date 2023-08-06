2c22622384ad1bba13b78951ab8b6a80c4ffd3e5
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-08-06 22:06:14 +0000

    setting up repo

---
+++ factory/models/base_model.py
@@ -0,0 +1,19 @@
+#!/urs/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """This is representing the BaseModel of the hbnb project"""
+
+    # The __init__() method initializes a new BaseModel instance
+    def __init__(self, *args, **kvargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+           
+        pass
