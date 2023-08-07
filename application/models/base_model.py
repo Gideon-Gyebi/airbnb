cf1047a77a22a545307089d5c7c2d7f31adf9e33
Gideon Gyebi <gyebigideon000@gmail.com>
2023-08-07 15:41:07 -0700

    Fixing bug on no attribute for (storage)

--- application/models/base_model.py
+++ application/models/base_model.py
@@ -4,11 +4,12 @@ import models
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """This is representing the BaseModel of the hbnb project"""
 
     # The __init__() method initializes a new BaseModel instance
-    def __init__(self, *args, **kvargs):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
 
         Args:
@@ -26,4 +27,27 @@ class BaseModel:
                 else:
                     self.__dict__[k] = v
         else:
-                    models.storage.new(self)
+            models.storage.new(self)
+
+    def save(self):
+        """Updating updated_at with the current datetime
+        and save the instance."""
+        self.update_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returning the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Returning the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
